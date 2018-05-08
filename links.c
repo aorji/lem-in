ae692a6d4eb23a651e46c86c9c0db5badb45d113
Anastasiia ORJI <aorji@e2r10p5.unit.ua>
2018-05-08 19:37:12 +0300

    find links

--- links.c
+++ links.c
@@ -12,48 +12,35 @@
 
 #include "lem-in.h"
 
-// static t_node	*create_link(t_node *node, char *name, char *link)
-// {
-// 	// t_node	*head;
+static int	create_link(t_node **node, char *name, char *link)
+{
+	t_node	*head;
 
-// 	// head = node;
-// 	// node->kid = (t_kids *)malloc(sizeof(t_kids));
-// 	// node->kid->next = NULL;
-// 	// printf("start with %s\n", node->name);
-// 	// while (node->next)
-// 	// {
-// 	// 	if (!ft_strcmp(name, node->name))
-// 	// 	{
-// 	// 		printf("node = %s\n", node->name);
-// 	// 		if (node)
-// 	// 			node->kid->name = "1"; //ft_strcpy(ft_strnew(ft_strlen(link)), link);
-// 	// 		else
-// 	// 			printf("%s\n", "????");
-// 	// 		// (*node)->kid->next = (t_kids *)malloc(sizeof(t_kids));
-// 	// 		// printf("kid = %s\n", (*node)->kid->name);
-// 	// 		// (*node)->kid = (*node)->kid->next;
-// 	// 		// (*node)->kid->next = NULL;
-// 	// 		return (head);
-// 	// 	}
-// 	// 	node = node->next;
-// 	// }
-// 	// error();
-// 	// return (head);
-// }
+	head = *node;
+	while (head->next)
+	{
+		if (!ft_strcmp(head->name, name))
+		{
+			printf("%s-%s\n", name, link);
+			return (1);
+		}
+		head = head->next;
+	}
+	error();
+	return (1);
+}
 
 int			link_info(int fd, char *line, t_node **node)
 {
 	char	**arr;
-	// t_node	*head;
 
-	// head = *node;
+	printf("line = %s\n", line);
 	arr = ft_strsplit(line, '-');
-	// if (arrlen(arr) == 2)
-	// {
-	// 	// *node = create_link(head, arr[0], arr[1]);
-	// 	// *node = create_link(head, arr[1], arr[0]);
-
-	// }
+	if (arrlen(arr) == 2)
+	{
+		create_link(node, arr[0], arr[1]);
+		create_link(node, arr[1], arr[0]);
+	}
 	if ((arrlen(arr) != 2) && !(line[0] == '#') && (line[1] != '#')) 
 		error();
 	else if (get_next_line(fd, &line) == 1)
